d1af54062063ea932916291898011de082a8afeb
Caleb Palmer <palmer.caleb@gmail.com>
2015-08-01 08:32:35 -0300

    Added macro CARTESIAN_Y_TO_WINDOW_Y(H, Y) to flip y coords from cartesian to window

--- capcommon.h
+++ capcommon.h
@@ -13,6 +13,8 @@ namespace CapEngine {
 #ifdef OSX
 #define LINESEP "\r"
 #endif
+
+#define CARTESIAN_Y_TO_WINDOW_Y(H, Y) H - Y
   
   void sleep(int ms);
   std::string strip(const std::string str);
