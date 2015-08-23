4d15d69c06a6321b5599119a18110bf86295efc2
Caleb Palmer <palmer.caleb@gmail.com>
2015-08-23 17:55:29 -0300

    Fixes issue including colour.h and adds default value for alpha value

--- colour.h
+++ colour.h
@@ -1,14 +1,12 @@
 #ifndef COLOUR_H
-#define COLOUR_J
+#define COLOUR_H
 
 #include <cinttypes>
 
 namespace CapEngine{
   class Colour{
   public:
-    Colour(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
-
-  private:
+    Colour(uint8_t r, uint8_t g, uint8_t b, uint8_t a = 0xFF);
     uint8_t m_r;
     uint8_t m_g;
     uint8_t m_b;
