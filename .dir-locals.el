011648645755b3e2585ca1eb5d3602dec45f744c
Caleb Palmer <palmer.caleb@gmail.com>
2019-01-12 12:22:13 -0400

    Changes to build system

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,7 +4,10 @@
 ((nil
   ;;(compile-command . "gtags && cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 . && rc -J && make"))
   ;;(compile-command . "make -j4"))
-  (compile-command . "cmake --build build"))
+  ;;(compile-command . "cmake --build build")
+  (if
+      (not (boundp 'cap/project_loaded))
+      (progn (load-file "./init.el") (setq cap/project-loaded "t"))))
  (c++-mode
   (tab-width . 2))
   (c-basic-offset . 2) 
