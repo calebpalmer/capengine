63136ebc7922f7934f3379f296fb635e30e4ed88
Caleb Palmer <palmer.caleb@gmail.com>
2020-05-23 18:13:35 -0300

    Fixes some warnings.

--- components.h
+++ components.h
@@ -7,59 +7,59 @@
 #include <optional>
 
 namespace CapEngine {
-class GameObject;
+  class GameObject;
 
-//! The component type.
-enum class ComponentType {
-  Physics,
-	Graphics,
-	Input,
-	Custom,
-	AI
-};
+  //! The component type.
+  enum class ComponentType {
+			    Physics,
+			    Graphics,
+			    Input,
+			    Custom,
+			    AI
+  };
 
-class Component{
-public:
-	virtual ~Component() = default;
-	
-	virtual void update(GameObject* object, double timestep) = 0;
-	virtual void receive(GameObject* object, int messageId, std::string message) {}
-	virtual ComponentType getType() const = 0;
+  class Component{
+  public:
+    virtual ~Component() = default;
 
-	virtual std::unique_ptr<Component> clone() const = 0;
-};
-  
-class PhysicsComponent : public Component {
-public:
-	~PhysicsComponent() override = default;
-	virtual std::optional<Rectangle> boundingPolygon(const GameObject* object) const { return std::nullopt; }
-	ComponentType getType() const override { return ComponentType::Physics; }
-};
+    virtual void update(GameObject* object, double timestep) = 0;
+    virtual void receive(GameObject* /*object*/, int /*messageId*/, std::string /*message*/) {}
+    virtual ComponentType getType() const = 0;
 
-class GraphicsComponent : public Component {
-public:
-	~GraphicsComponent() override = default;
-	virtual void render(GameObject* object, const Camera2d &in_camera, uint32_t in_windowId) = 0;
-	ComponentType getType() const override { return ComponentType::Graphics; }
-};
+    virtual std::unique_ptr<Component> clone() const = 0;
+  };
 
-class InputComponent : public Component {
-public:
-	~InputComponent() override = default;
-	ComponentType getType() const override { return ComponentType::Input; }
-};
+  class PhysicsComponent : public Component {
+  public:
+    ~PhysicsComponent() override = default;
+    virtual std::optional<Rectangle> boundingPolygon(const GameObject* /*object*/) const { return std::nullopt; }
+    ComponentType getType() const override { return ComponentType::Physics; }
+  };
 
-class CustomComponent : public Component {
-public:
-	~CustomComponent() override = default;
-	ComponentType getType() const override { return ComponentType::Custom; }
-};
+  class GraphicsComponent : public Component {
+  public:
+    ~GraphicsComponent() override = default;
+    virtual void render(GameObject* object, const Camera2d &in_camera, uint32_t in_windowId) = 0;
+    ComponentType getType() const override { return ComponentType::Graphics; }
+  };
 
-class AIComponent : public Component {
-public:
-	~AIComponent() override = default;
-	ComponentType getType() const override { return ComponentType::AI; }
-};
+  class InputComponent : public Component {
+  public:
+    ~InputComponent() override = default;
+    ComponentType getType() const override { return ComponentType::Input; }
+  };
+
+  class CustomComponent : public Component {
+  public:
+    ~CustomComponent() override = default;
+    ComponentType getType() const override { return ComponentType::Custom; }
+  };
+
+  class AIComponent : public Component {
+  public:
+    ~AIComponent() override = default;
+    ComponentType getType() const override { return ComponentType::AI; }
+  };
 
 }
 
