2071062400e925654a7e73d913e9ca97d285ef61
Caleb Palmer <palmer.caleb@gmail.com>
2020-05-26 07:02:22 -0300

    Fix formatting.

--- asset_manager.h
+++ asset_manager.h
@@ -16,78 +16,78 @@
 
 namespace CapEngine{
 
-struct Frame {
-	std::string frameName;
-	int rowNum;
-	int frameWidth;
-	int frameHeight;
-	int numFrames;
-	double animationTime;
-	int horizontalPadding;
-	int verticalPadding;
-};
+  struct Frame {
+    std::string frameName;
+    int rowNum;
+    int frameWidth;
+    int frameHeight;
+    int numFrames;
+    double animationTime;
+    int horizontalPadding;
+    int verticalPadding;
+  };
 
-struct Image {
-	std::string path;
-	Texture* texture;
-	std::map<std::string, Frame> frames;
-};
+  struct Image {
+    std::string path;
+    Texture* texture;
+    std::map<std::string, Frame> frames;
+  };
 
-struct SoftwareImage{
-	std::string path;
-	Surface* surface;
-};
+  struct SoftwareImage{
+    std::string path;
+    Surface* surface;
+  };
 
-struct Sound {
-	std::string path;
-	PCM* pcm;
-};
+  struct Sound {
+    std::string path;
+    PCM* pcm;
+  };
 
-struct AssetDoesNotExistError : public CapEngineException {
-	AssetDoesNotExistError(std::string_view in_type, int in_assetId) :
-		CapEngineException([&]()
-											 {
-												 std::stringstream stream;
-												 stream << "The " << in_type << " with id " << in_assetId << " does not exist.";
-												 return stream.str();
-											 }()) {}
-};
+  struct AssetDoesNotExistError : public CapEngineException {
+    AssetDoesNotExistError(std::string_view in_type, int in_assetId) :
+      CapEngineException([&]()
+			 {
+			   std::stringstream stream;
+			   stream << "The " << in_type << " with id " << in_assetId << " does not exist.";
+			   return stream.str();
+			 }()) {}
+  };
   
-class AssetManager{
+  class AssetManager{
     
-public:
-	AssetManager(VideoManager& videoManager, SoundPlayer& soundPlayer, const std::string &assetFile);
-	~AssetManager();
-	void draw(Uint32 windowID, int id, Rectangle srcRect, Rectangle dstRect);
-	void draw(Uint32 windowID, int id, Rectangle dstRect, int row, int frame);
-	void draw(Uint32 windowID, int id, Vector position);
-	void draw(Uint32 windowID, int id, Rectangle dstRect);
-	void loadImage(int id, std::string path, int frameWidth=0, int frameHeight=0);
-	Image* getImage(int id);
-	SoftwareImage getSoftwareImage(int id);
-	bool imageExists(int id) const;
-	int getImageWidth(int id);
-	int getImageHeight(int id);
-	Frame getFrame(int assetID, std::string frameName);
-	Frame getFrame(int assetID, int rowNum);
+  public:
+    AssetManager(VideoManager& videoManager, SoundPlayer& soundPlayer, const std::string &assetFile);
+    ~AssetManager();
+    void draw(Uint32 windowID, int id, Rectangle srcRect, Rectangle dstRect);
+    void draw(Uint32 windowID, int id, Rectangle dstRect, int row, int frame);
+    void draw(Uint32 windowID, int id, Vector position);
+    void draw(Uint32 windowID, int id, Rectangle dstRect);
+    void loadImage(int id, std::string path, int frameWidth=0, int frameHeight=0);
+    Image* getImage(int id);
+    SoftwareImage getSoftwareImage(int id);
+    bool imageExists(int id) const;
+    int getImageWidth(int id);
+    int getImageHeight(int id);
+    Frame getFrame(int assetID, std::string frameName);
+    Frame getFrame(int assetID, int rowNum);
 
-	long playSound(int id, bool repeat=false);
-	void stopSound(int id);
-	void loadSound(int id, std::string path);
-	Sound* getSound(int id);
-	bool soundExists(int id) const;
+    long playSound(int id, bool repeat=false);
+    void stopSound(int id);
+    void loadSound(int id, std::string path);
+    Sound* getSound(int id);
+    bool soundExists(int id) const;
 
-private: 
-	std::map<int, Image> mImageMap;
-	std::map<int, Sound> mSoundMap;
-	VideoManager& mVideoManager;
-	SoundPlayer& mSoundPlayer;
-	std::string mAssetFile;
+  private: 
+    std::map<int, Image> mImageMap;
+    std::map<int, Sound> mSoundMap;
+    VideoManager& mVideoManager;
+    SoundPlayer& mSoundPlayer;
+    std::string mAssetFile;
 
-private: //functions
-	void parseAssetFile(XmlParser& parser);
+  private: //functions
+    void parseAssetFile(XmlParser& parser);
 
-};
+  };
 
 }
 #endif // ASSET_MANAGER_H
