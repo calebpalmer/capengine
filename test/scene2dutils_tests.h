7b2e361478ec2bc14ca364ebe1a182f06410f66a
Caleb Palmer <palmer.caleb@gmail.com>
2021-08-27 07:38:15 -0300

    Fix memory leak.

--- test/scene2dutils_tests.h
+++ test/scene2dutils_tests.h
@@ -4,8 +4,10 @@
 #include <cppunit/TestCase.h>
 #include <cppunit/TestSuite.h>
 #include <cppunit/extensions/HelperMacros.h>
+#include <variant>
 
 #include "../scene2dutils.h"
+#include "cppunit/TestAssert.h"
 
 namespace CapEngine
 {
@@ -14,29 +16,40 @@ namespace Testing
 
 class Scene2dUtilsTest : public CppUnit::TestFixture
 {
-public:
-  void testReadRectangle();
+  public:
+    void testReadRectangle();
+    void testReadComponentMetadata();
 
-  CPPUNIT_TEST_SUITE(Scene2dUtilsTest);
-  CPPUNIT_TEST(testReadRectangle);
-  CPPUNIT_TEST_SUITE_END();
+    CPPUNIT_TEST_SUITE(Scene2dUtilsTest);
+    CPPUNIT_TEST(testReadRectangle);
+    CPPUNIT_TEST(testReadComponentMetadata);
+    CPPUNIT_TEST_SUITE_END();
 };
 
 //! Tests reading a rectangle from json.
 void Scene2dUtilsTest::testReadRectangle()
 {
-  jsoncons::json rect = jsoncons::json::parse(R"(
+    jsoncons::json rect = jsoncons::json::parse(R"(
 {
-    "x" : 1,
-    "y" : 2,
-    "width" : 10,
-    "height" : 10
+		"x" : 1,
+		"y" : 2,
+		"width" : 10,
+		"height" : 10
 }
 )");
 
-  Rectangle expected(1, 2, 10, 10);
-  Rectangle actual = JSONUtils::readRectangle(rect);
-  CPPUNIT_ASSERT_EQUAL(expected, actual);
+    Rectangle expected(1, 2, 10, 10);
+    Rectangle actual = JSONUtils::readRectangle(rect);
+    CPPUNIT_ASSERT_EQUAL(expected, actual);
+}
+
+//! Test reading component metadata
+void Scene2dUtilsTest::testReadComponentMetadata()
+{
+    jsoncons::json intData(1);
+    const Component::MetadataType value = JSONUtils::readMetadata(intData);
+    CPPUNIT_ASSERT_NO_THROW(std::get<int>(value));
+    CPPUNIT_ASSERT_EQUAL(1, std::get<int>(value));
 }
 
 } // namespace Testing
--- xml_parser.cpp
+++ xml_parser.cpp
@@ -2,8 +2,10 @@
 
 #include "CapEngineException.h"
 #include "capcommon.h"
+#include "libxml/xpath.h"
 
 #include <iostream>
+#include <memory>
 #include <sstream>
 
 using namespace std;
@@ -11,108 +13,116 @@ using namespace CapEngine;
 
 XmlParser::XmlParser(const string xmlPath) : mXmlPath(xmlPath)
 {
-  mpDoc = xmlParseFile(xmlPath.c_str());
-  if (mpDoc == NULL) {
-    ostringstream errorMessage;
-    errorMessage << "Unable to parse file " << mXmlPath;
-    throw CapEngineException(errorMessage.str());
-  }
+    mpDoc = xmlParseFile(xmlPath.c_str());
+    if (mpDoc == NULL) {
+        ostringstream errorMessage;
+        errorMessage << "Unable to parse file " << mXmlPath;
+        throw CapEngineException(errorMessage.str());
+    }
 }
 
 XmlParser::~XmlParser()
 {
-  if (mpDoc)
-    xmlFreeDoc(mpDoc);
-  xmlCleanupParser();
+    if (mpDoc)
+        xmlFreeDoc(mpDoc);
+
+    xmlCleanupParser();
 }
 
 vector<XmlNode> XmlParser::getNodes(const string _expression)
 {
-  mpXPathContext = xmlXPathNewContext(mpDoc);
+    // mpXPathContext = xmlXPathNewContext(mpDoc);
+    std::unique_ptr<xmlXPathContext, decltype(&xmlXPathFreeContext)>
+        xPathContext(xmlXPathNewContext(mpDoc),
+                     [](xmlXPathContextPtr in_contextPtr) {
+                         xmlXPathFreeContext(in_contextPtr);
+                     });
 
-  // need to cast off the const from string::c_str() and then cast to xmlChar*
-  xmlChar *expression =
-      reinterpret_cast<xmlChar *>(const_cast<char *>(_expression.c_str()));
-  xmlXPathObjectPtr result = xmlXPathEvalExpression(expression, mpXPathContext);
+    // need to cast off the const from string::c_str() and then cast to xmlChar*
+    xmlChar *expression =
+        reinterpret_cast<xmlChar *>(const_cast<char *>(_expression.c_str()));
+    xmlXPathObjectPtr result =
+        xmlXPathEvalExpression(expression, xPathContext.get());
 
-  // iterate through xpath results and add to vector
-  int i;
-  xmlNodeSetPtr nodeset = result->nodesetval;
-  vector<XmlNode> nodes;
-  for (i = 0; i < nodeset->nodeNr; i++) {
-    // not sure of the ownership of the XmlNodePtr in nodeset->nodeTab[i]
-    // maybe it lasts for as long as the xmlDoc exists?
-    nodes.push_back(nodeset->nodeTab[i]);
-  }
+    // iterate through xpath results and add to vector
+    int i;
+    xmlNodeSetPtr nodeset = result->nodesetval;
+    vector<XmlNode> nodes;
+    for (i = 0; i < nodeset->nodeNr; i++) {
+        // not sure of the ownership of the XmlNodePtr in nodeset->nodeTab[i]
+        // maybe it lasts for as long as the xmlDoc exists?
+        nodes.push_back(nodeset->nodeTab[i]);
+    }
 
-  xmlXPathFreeObject(result);
-  return nodes;
+    xmlXPathFreeObject(result);
+    return nodes;
 }
 
 XmlNode XmlParser::getRoot()
 {
-  XmlNode root = xmlDocGetRootElement(mpDoc);
-  if (root == NULL) {
-    throw CapEngineException("Could not get root node");
-  }
-  return root;
+    XmlNode root = xmlDocGetRootElement(mpDoc);
+    if (root == NULL) {
+        throw CapEngineException("Could not get root node");
+    }
+    return root;
 }
 
 string XmlParser::getNodeName(XmlNode node)
 {
-  if (node == nullptr) {
-    throw CapEngineException("Node is invalid");
-  }
-  // this should use c++ style casts
-  string name = (char *)node->name;
-  return name;
+    if (node == nullptr) {
+        throw CapEngineException("Node is invalid");
+    }
+    // this should use c++ style casts
+    string name = (char *)node->name;
+    return name;
 }
 
 vector<XmlNode> XmlParser::getNodeChildren(XmlNode node)
 {
-  if (node == nullptr) {
-    throw CapEngineException("Invalid node");
-  }
-  vector<XmlNode> children;
-  XmlNode currentChild = node->xmlChildrenNode;
-  while (currentChild != nullptr) {
-    if (currentChild->type == XML_ELEMENT_NODE) {
-      children.push_back(currentChild);
+    if (node == nullptr) {
+        throw CapEngineException("Invalid node");
     }
-    currentChild = currentChild->next;
-  }
+    vector<XmlNode> children;
+    XmlNode currentChild = node->xmlChildrenNode;
+    while (currentChild != nullptr) {
+        if (currentChild->type == XML_ELEMENT_NODE) {
+            children.push_back(currentChild);
+        }
+        currentChild = currentChild->next;
+    }
 
-  return children;
+    return children;
 }
 
 XmlNode XmlParser::getNextNode(XmlNode node) { return node->next; }
 
 string XmlParser::getStringValue(XmlNode node)
 {
-  string value = (char *)xmlNodeListGetString(mpDoc, node->xmlChildrenNode, 1);
-  value = strip(value);
-  return value;
+    string value =
+        (char *)xmlNodeListGetString(mpDoc, node->xmlChildrenNode, 1);
+    value = strip(value);
+    return value;
 }
 
 string XmlParser::getAttribute(XmlNode node, const string key)
 {
-  xmlChar *value;
-  value = xmlGetProp(node, (xmlChar *)key.c_str());
-  string retVal;
-  if (value == nullptr) {
-    retVal = "";
-  } else {
-    retVal = (char *)value;
-    xmlFree(value);
-  }
-  return retVal;
+    xmlChar *value;
+    value = xmlGetProp(node, (xmlChar *)key.c_str());
+    string retVal;
+    if (value == nullptr) {
+        retVal = "";
+    } else {
+        retVal = (char *)value;
+        xmlFree(value);
+    }
+    return retVal;
 }
 
 bool XmlParser::nodeNameCompare(XmlNode node, const std::string name)
 {
-  if (!xmlStrcmp(node->name, (const xmlChar *)name.c_str())) {
-    return true;
-  } else {
-    return false;
-  }
+    if (!xmlStrcmp(node->name, (const xmlChar *)name.c_str())) {
+        return true;
+    } else {
+        return false;
+    }
 }
