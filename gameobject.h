ef475e1ff94b765161cbf0250eff715ac931ac57
Caleb Palmer <palmer.caleb@gmail.com>
2022-07-11 20:44:36 -0300

    Updates for tanks

--- gameobject.h
+++ gameobject.h
@@ -18,29 +18,25 @@ namespace CapEngine
 class Camera2d;
 class GameObject;
 
-class CollisionEvent
-{
-  public:
-    std::shared_ptr<GameObject> object1;
-    std::shared_ptr<GameObject> object2;
-    CollisionType type;
-    CollisionClass class_;
+class CollisionEvent {
+public:
+  std::shared_ptr<GameObject> object1;
+  std::shared_ptr<GameObject> object2;
+  CollisionType type;
+  CollisionClass class_;
 
-    friend std::ostream &operator<<(std::ostream &stream,
-                                    const CollisionEvent collisionEvent);
+  friend std::ostream &operator<<(std::ostream &stream, const CollisionEvent collisionEvent);
 };
 
-class ObjectCreator
-{
-  public:
-    virtual std::unique_ptr<GameObject> createObject() = 0;
-    virtual ~ObjectCreator() {}
+class ObjectCreator {
+public:
+  virtual std::unique_ptr<GameObject> createObject() = 0;
+  virtual ~ObjectCreator() {}
 };
 
-class ObjectData
-{
-  public:
-    virtual ~ObjectData(){};
+class ObjectData {
+public:
+  virtual ~ObjectData(){};
 };
 
 typedef long ObjectID;
@@ -48,124 +44,116 @@ typedef long ObjectID;
 //! Whether coordinate system as top is 0 or bottom is 0
 enum class YAxisOrientation { TopZero, BottomZero };
 
-class GameObject
-{
-  public:
-    enum ObjectState { Inactive, Starting, Active, Dying, Dead };
+class GameObject {
+public:
+  enum ObjectState { Inactive, Starting, Active, Dying, Dead };
 
-    enum ObjectType { ObjectType_AI, ObjectType_Player };
+  enum ObjectType { ObjectType_AI, ObjectType_Player };
 
-    // constructors
-    GameObject(bool newID = true);
+  // constructors
+  GameObject(bool newID = true);
 
-    ~GameObject() = default;
-    GameObject(const GameObject &);
-    GameObject &operator=(const GameObject &);
-    GameObject(GameObject &&in_other);
-    GameObject &operator=(GameObject &&in_other);
+  ~GameObject() = default;
+  GameObject(const GameObject &);
+  GameObject &operator=(const GameObject &);
+  GameObject(GameObject &&in_other);
+  GameObject &operator=(GameObject &&in_other);
 
-    void swap(GameObject &io_other) noexcept;
+  void swap(GameObject &io_other) noexcept;
 
-    static ObjectID generateID();
-    static int generateMessageId();
-    void render(const Camera2d &in_camera, uint32_t in_windowId);
-    std::unique_ptr<GameObject> update(double ms) const;
-    Rectangle boundingPolygon() const;
-    bool handleCollision(CollisionType, CollisionClass, GameObject *otherObject,
-                         Vector collisionLocation);
-    std::unique_ptr<GameObject> clone() const;
-    // getters and setters
-    std::shared_ptr<ObjectData> getObjectData() const;
-    void setObjectData(std::shared_ptr<ObjectData> pObjectData);
-    ObjectState getObjectState() const;
-    void setObjectState(ObjectState objectState);
-    ObjectID getObjectID() const;
-    void setObjectID(ObjectID id);
-    ObjectID getParentObjectID() const;
-    void send(int messageId, const std::string &message);
-    void setParentObjectID(ObjectID id);
+  static ObjectID generateID();
+  static int generateMessageId();
+  void render(const Camera2d &in_camera, uint32_t in_windowId);
+  std::unique_ptr<GameObject> update(double ms) const;
+  Rectangle boundingPolygon() const;
+  bool handleCollision(CollisionType, CollisionClass, GameObject *otherObject, Vector collisionLocation);
+  std::unique_ptr<GameObject> clone() const;
+  // getters and setters
+  std::shared_ptr<ObjectData> getObjectData() const;
+  void setObjectData(std::shared_ptr<ObjectData> pObjectData);
+  ObjectState getObjectState() const;
+  void setObjectState(ObjectState objectState);
+  ObjectID getObjectID() const;
+  void setObjectID(ObjectID id);
+  ObjectID getParentObjectID() const;
+  void send(int messageId, const std::string &message);
+  void setParentObjectID(ObjectID id);
 
-    void addComponent(std::shared_ptr<Component> in_pComponent);
-    const std::vector<std::shared_ptr<Component>> &getComponents();
-    std::vector<std::shared_ptr<Component>>
-        getComponents(ComponentType in_type);
-    template <typename T> std::vector<std::shared_ptr<T>> getComponents();
+  void addComponent(std::shared_ptr<Component> in_pComponent);
+  const std::vector<std::shared_ptr<Component>> &getComponents();
+  std::vector<std::shared_ptr<Component>> getComponents(ComponentType in_type);
+  template <typename T> std::vector<std::shared_ptr<T>> getComponents();
 
-    Vector const &getPosition() const;
-    void setPosition(Vector position);
-    Vector const &getOrientation() const;
-    void setOrientation(Vector orientation);
-    Vector const &getVelocity() const;
-    void setVelocity(Vector velocity);
-    Vector const &getAcceleration() const;
-    void setAcceleration(Vector velocity);
-    Vector const &getPreviousPosition() const;
-    void setPreviousPosition(Vector position);
-    Vector const &getForce() const;
-    void setForce(Vector in_force);
+  Vector const &getPosition() const;
+  void setPosition(Vector position);
+  Vector const &getOrientation() const;
+  void setOrientation(Vector orientation);
+  Vector const &getVelocity() const;
+  void setVelocity(Vector velocity);
+  Vector const &getAcceleration() const;
+  void setAcceleration(Vector velocity);
+  Vector const &getPreviousPosition() const;
+  void setPreviousPosition(Vector position);
+  Vector const &getForce() const;
+  void setForce(Vector in_force);
 
-    ObjectType getObjectType() const;
-    void setObjectType(ObjectType in_objectType);
+  ObjectType getObjectType() const;
+  void setObjectType(ObjectType in_objectType);
 
-    YAxisOrientation getYAxisOrientation() const;
-    void setYAxisOrientation(YAxisOrientation in_orientation);
+  YAxisOrientation getYAxisOrientation() const;
+  void setYAxisOrientation(YAxisOrientation in_orientation);
 
-    friend std::ostream &operator<<(std::ostream &stream,
-                                    GameObject const &object);
+  friend std::ostream &operator<<(std::ostream &stream, GameObject const &object);
 
-    using Metadata = std::map<std::string, MetadataType>;
-    Metadata const &metadata() const;
-    void setMetadata(std::string const &in_key, MetadataType const &in_value);
+  using Metadata = std::map<std::string, MetadataType>;
+  Metadata const &metadata() const;
+  void setMetadata(std::string const &in_key, MetadataType const &in_value);
 
-  private:
-    static ObjectID nextID;
-    static int nextMessageId;
-    std::shared_ptr<ObjectData> m_pObjectData;
-    ObjectState m_objectState = Active;
-    ObjectID m_objectID = -1;
-    ObjectID m_parentObjectID = -1;
-    ObjectType m_objectType = ObjectType_AI;
-    YAxisOrientation m_yAxisOrientation = YAxisOrientation::BottomZero;
+private:
+  static ObjectID nextID;
+  static int nextMessageId;
+  std::shared_ptr<ObjectData> m_pObjectData;
+  ObjectState m_objectState = Active;
+  ObjectID m_objectID = -1;
+  ObjectID m_parentObjectID = -1;
+  ObjectType m_objectType = ObjectType_AI;
+  YAxisOrientation m_yAxisOrientation = YAxisOrientation::BottomZero;
 
-    //! metadata
-    Metadata m_metadata;
+  //! metadata
+  Metadata m_metadata;
 
-    //! The components
-    std::vector<std::shared_ptr<Component>> m_components;
+  //! The components
+  std::vector<std::shared_ptr<Component>> m_components;
 
-    Vector position;
-    Vector previousPosition;
-    Vector orientation;
-    Vector velocity;
-    Vector acceleration;
-    Vector force;
+  Vector position;
+  Vector previousPosition;
+  Vector orientation;
+  Vector velocity;
+  Vector acceleration;
+  Vector force;
 };
 
-template <typename T>
-std::vector<std::shared_ptr<T>> GameObject::getComponents()
-{
-    std::vector<std::shared_ptr<T>> components;
-    for (auto &&component : m_components) {
-        auto castedObject = std::dynamic_pointer_cast<T>(component);
-        if (castedObject != nullptr)
-            components.push_back(castedObject);
-    }
+template <typename T> std::vector<std::shared_ptr<T>> GameObject::getComponents() {
+  std::vector<std::shared_ptr<T>> components;
+  for (auto &&component : m_components) {
+    auto castedObject = std::dynamic_pointer_cast<T>(component);
+    if (castedObject)
+      components.push_back(castedObject);
+  }
 }
 
 // events
-class GameObjectStateChangedEvent : public GameEvent
-{
-  public:
-    GameObjectStateChangedEvent(GameObject *object,
-                                GameObject::ObjectState stateBefore,
-                                GameObject::ObjectState stateAfter);
-    ~GameObjectStateChangedEvent() override = default;
-    std::string type() const override { return std::string(kType); };
+class GameObjectStateChangedEvent : public GameEvent {
+public:
+  GameObjectStateChangedEvent(GameObject *object, GameObject::ObjectState stateBefore,
+                              GameObject::ObjectState stateAfter);
+  ~GameObjectStateChangedEvent() override = default;
+  std::string type() const override { return std::string(kType); };
 
-    static constexpr char kType[] = "GameObjectSateChanged";
-    GameObject *m_object;
-    GameObject::ObjectState m_stateBefore;
-    GameObject::ObjectState m_stateAfter;
+  static constexpr char kType[] = "GameObjectSateChanged";
+  GameObject *m_object;
+  GameObject::ObjectState m_stateBefore;
+  GameObject::ObjectState m_stateAfter;
 };
 
 } // namespace CapEngine
--- logger.h
+++ logger.h
@@ -1,58 +1,53 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Weverything"
+//#pragma clang diagnostic push
+//#pragma clang diagnostic ignored "-Weverything"
 #include <iostream>
 #include <jsoncons/json.hpp>
 #include <ostream>
 #include <string>
-#pragma clang diagnostic pop
+//#pragma clang diagnostic pop
 
-#define CAP_LOG(logger, msg, level)                                            \
-    assert(logger != nullptr);                                                 \
-    logger->log(msg, level, __FILE__, __LINE__);
+#define CAP_LOG(logger, msg, level)                                                                                    \
+  assert(logger != nullptr);                                                                                           \
+  logger->log(msg, level, __FILE__, __LINE__);
 
-#define CAP_LOG_SDL_ERROR(logger, level)                                       \
-    assert(logger != nullptr);                                                 \
-    logger->logSDLError(level, __FILE__, __LINE__);
+#define CAP_LOG_SDL_ERROR(logger, level)                                                                               \
+  assert(logger != nullptr);                                                                                           \
+  logger->logSDLError(level, __FILE__, __LINE__);
 
-#define CAP_LOG_EXCEPTION(logger, e, level)                                    \
-    assert(logger != nullptr);                                                 \
-    logger->log(e, level, __FILE__, __LINE__);
+#define CAP_LOG_EXCEPTION(logger, e, level)                                                                            \
+  assert(logger != nullptr);                                                                                           \
+  logger->log(e, level, __FILE__, __LINE__);
 
-namespace CapEngine
-{
+namespace CapEngine {
 
-class Logger
-{
-    static bool instantiated;
-    std::ostream *outputStream;
+class Logger {
+  static bool instantiated;
+  std::ostream *outputStream;
 
-  public:
-    enum LogLevel {
-        CNONE = 0,
-        CDEBUG,
-        CWARNING,
-        CERROR,
-    };
+public:
+  enum LogLevel {
+    CNONE = 0,
+    CDEBUG,
+    CWARNING,
+    CERROR,
+  };
 
-    LogLevel currentLevel;
+  LogLevel currentLevel;
 
-    Logger();
-    ~Logger();
+  Logger();
+  ~Logger();
 
-    void log(const std::string &message, LogLevel level = CWARNING,
-             char const *file = "", int lineno = -1);
-    void log(const std::exception &e, LogLevel level = CWARNING,
-             char const *file = "", int lineno = -1);
-    void log(const jsoncons::json_exception &e, LogLevel = CWARNING,
-             char const *file = "", int lineno = -1);
-    void logSDLError(LogLevel level, char const *file = "", int lineno = -1);
-    void setOutput(std::ostream &stream);
+  void log(const std::string &message, LogLevel level = CWARNING, char const *file = "", int lineno = -1);
+  void log(const std::exception &e, LogLevel level = CWARNING, char const *file = "", int lineno = -1);
+  void log(const jsoncons::json_exception &e, LogLevel = CWARNING, char const *file = "", int lineno = -1);
+  void logSDLError(LogLevel level, char const *file = "", int lineno = -1);
+  void setOutput(std::ostream &stream);
 
-  private:
-    std::string getLevelDescription(LogLevel level);
+private:
+  std::string getLevelDescription(LogLevel level);
 };
 
 } // namespace CapEngine
